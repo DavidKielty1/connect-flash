7ffa6f551def7ca0abedaf170bdbcfb154223d7c
DavidKielty1 <davidkielty@live.co.uk>
2021-05-27 11:44:40 +0100

    async error callbacks middleware monogo etc next try catch next throw

--- index.js
+++ index.js
@@ -27,14 +27,18 @@ app.use(methodOverride("_method"));
 
 const categories = ["fruit", "vegetable", "dairy", "mushrooms"];
 
-app.get("/products", async (req, res) => {
-  const { category } = req.query;
-  if (category) {
-    const products = await Product.find({ category });
-    res.render("products/index", { products, category });
-  } else {
-    const products = await Product.find({});
-    res.render("products/index", { products, category: "All" });
+app.get("/products", async (req, res, next) => {
+  try {
+    const { category } = req.query;
+    if (category) {
+      const products = await Product.find({ category });
+      res.render("products/index", { products, category });
+    } else {
+      const products = await Product.find({});
+      res.render("products/index", { products, category: "All" });
+    }
+  } catch (e) {
+    next(e);
   }
 });
 
@@ -42,38 +46,54 @@ app.get("/products/new", (req, res) => {
   res.render("products/new", { categories });
 });
 
-app.post("/products", async (req, res) => {
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  console.log(newProduct);
-  res.redirect(`/products/${newProduct._id}`);
+app.post("/products", async (req, res, next) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.redirect(`/products/${newProduct._id}`);
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.get("/products/:id", async (req, res, next) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  if (!product) {
-    return next(new AppError("No product with this ID", 404));
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      throw next(new AppError("No product with this ID", 404));
+    }
+    res.render("products/show", { product });
+  } catch (e) {
+    next(e);
   }
-  res.render("products/show", { product });
 });
 
 app.get("/products/:id/edit", async (req, res, next) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  if (!product) {
-    return next(new AppError("No product with this ID", 404));
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      throw next(new AppError("No product with this ID", 404));
+    }
+    res.render("products/edit", { product, categories });
+  } catch (e) {
+    next(e);
   }
-  res.render("products/edit", { product, categories });
 });
 
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-    new: true,
-  });
-  res.redirect(`/products/${product._id}`);
+app.put("/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    res.redirect(`/products/${product._id}`);
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.delete("/products/:id", async (req, res) => {
